85da6c33d32b8329d0b621dcf898ba185818b33b
aliahmedprositeyemen <a.ahmed@prositeyemen.com>
2026-01-07 01:13:49 +0300

    clean filed of alipro setting

--- alipro_theme/theme_alipro/doctype/alipro_setting/alipro_setting.py
+++ alipro_theme/theme_alipro/doctype/alipro_setting/alipro_setting.py
@@ -2,10 +2,4 @@ import re
 from frappe.model.document import Document
 
 class AliproSetting(Document):
-	def before_save(self):
-		if self.login_page_title is not None:
-			extra_spaces = re.search(r'^\s+', self.login_page_title)
-			if extra_spaces:
-				self.login_page_title=''
-			else:
-				pass
+	pass
